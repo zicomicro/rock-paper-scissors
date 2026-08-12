39f8da5fac63247264613e8d18506a6734093beb
Zakarya Al-Rafei <zico0micro@gmail.com>
2026-08-12 23:15:35 +0300

    feat(game): add getcomputerchoice function

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getComputerChoice()
+{
+    let result = Math.floor(Math.random() * 3);
+    if(result == 0){
+        return "rock";
+    }else if(result == 1){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
